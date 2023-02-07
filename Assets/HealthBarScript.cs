e98aaaa34aeec43a17ed674509a2659abd5f841f
WLGRest <78242454+WLGRest@users.noreply.github.com>
2023-02-07 14:14:08 -0500

    I'll stop here for now

---
+++ Assets/HealthBarScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarScript : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
