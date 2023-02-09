4cf8ca6948e36f2569e5d9ca2d23ae07a5378aed
Mark Schueler <schueler.mark.c@gmail.com>
2023-02-09 13:54:51 -0500

    spider web shooting and delay

--- Assets/scripts/spider.cs
+++ Assets/scripts/spider.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEditor;
 using UnityEngine;
 
 public class spider : Player
 {
+    public GameObject projectile;
+    public float launchVelocity = 700f;
+    private bool shot_taken = false; 
+
+
     // Start is called before the first frame update
-    void Start()
+    void Start()    
     {
         speed = 5;
     }
@@ -14,6 +21,19 @@ public class spider : Player
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire3") && shot_taken == false)
+        {
+            shot_taken = true;
+            Physics.IgnoreLayerCollision(8, 8, true);
+            GameObject web = Instantiate(projectile, transform.position, transform.rotation);
+            web.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, launchVelocity, 0));
+            StartCoroutine(Reload(5f));
+        }
         
+    }
+
+    private IEnumerator Reload(float f)
+    {
+        yield return new WaitForSeconds(f);
     }
 }
