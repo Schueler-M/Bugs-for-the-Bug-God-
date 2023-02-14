f5a18e0c9bd8de5efb691ba315a872583b8647a9
WLGRest <78242454+WLGRest@users.noreply.github.com>
2023-02-14 12:40:39 -0500

    Health now rotates with main camera

--- Assets/HealthBarScript.cs
+++ Assets/HealthBarScript.cs
@@ -7,8 +7,20 @@ public class HealthBarScript : MonoBehaviour
 {
     [SerializeField] private Image HealthSprite;
 
+    private Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
     public void UpdateHealth(float totalHealth, float curHealth)
     {
         HealthSprite.fillAmount = curHealth / totalHealth; // Setting the health percentage
+    }
+
+    void Update()
+    {
+        transform.rotation = camera.transform.rotation;
     }
 }
